d76bd1b8b529849e50a4ed7efc4ba2f1073ce96e
Vmiu <miuvictor21079@gmail.com>
2025-09-25 16:24:21 +0800

    progress part3 25/9 4:24pm

--- part3_edge_optimization.py
+++ part3_edge_optimization.py
@@ -10,6 +10,8 @@ import os
 os.makedirs('part3_edge_optimization', exist_ok=True)
 
 import time
+import numpy as np
+from itertools import product
 
 class EdgeOptimizer: 
 
@@ -94,20 +96,57 @@ class EdgeOptimizer:
 
         quantized_models = {} 
 
-         
+        # Representative dataset for calibration
+        def representative_data_gen():
+            for i in range(100):
+                yield [self.x_test[i:i+1].astype(np.float32)]
 
         # TODO: Implement dynamic range quantization 
         part3_logger.info(f"Implementing dynamic range quantization")
+        
+        # Dynamic range quantization - quantizes weights to int8 but keeps activations in float
+        converter_dynamic = tf.lite.TFLiteConverter.from_keras_model(self.baseline_model)
+        converter_dynamic.optimizations = [tf.lite.Optimize.DEFAULT]
+        dynamic_quantized_model = converter_dynamic.convert()
+        quantized_models['dynamic_range'] = dynamic_quantized_model
 
         # TODO: Implement full integer quantization 
         part3_logger.info(f"Implementing full integer quantization")
 
+        # Full integer quantization - quantizes both weights and activations to int8
+        converter_full_int = tf.lite.TFLiteConverter.from_keras_model(self.baseline_model)
+        converter_full_int.optimizations = [tf.lite.Optimize.DEFAULT]
+        converter_full_int.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+        converter_full_int.inference_input_type = tf.int8
+        converter_full_int.inference_output_type = tf.int8
+        
+        converter_full_int.representative_dataset = representative_data_gen
+        full_int_quantized_model = converter_full_int.convert()
+        quantized_models['full_integer'] = full_int_quantized_model
+
         # TODO: Implement float16 quantization 
         part3_logger.info(f"Implementing float16 quantization")
+        
+        # Float16 quantization - reduces precision to float16
+        converter_float16 = tf.lite.TFLiteConverter.from_keras_model(self.baseline_model)
+        converter_float16.optimizations = [tf.lite.Optimize.DEFAULT]
+        converter_float16.target_spec.supported_types = [tf.float16]
+        float16_quantized_model = converter_float16.convert()
+        quantized_models['float16'] = float16_quantized_model
         
         # TODO: Provide representative dataset for calibration 
         part3_logger.info(f"Providing representative dataset for calibration")
          
+        # Additional quantization with custom representative dataset
+        converter_calibrated = tf.lite.TFLiteConverter.from_keras_model(self.baseline_model)
+        converter_calibrated.optimizations = [tf.lite.Optimize.DEFAULT]
+        converter_calibrated.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+        converter_calibrated.inference_input_type = tf.int8
+        converter_calibrated.inference_output_type = tf.int8
+        
+        converter_calibrated.representative_dataset = representative_data_gen
+        calibrated_quantized_model = converter_calibrated.convert()
+        quantized_models['calibrated_int8'] = calibrated_quantized_model
 
         return quantized_models 
 
@@ -128,14 +167,40 @@ class EdgeOptimizer:
         """ 
 
         # TODO: Replace standard convolutions with depthwise separable convolutions 
-
-        # TODO: Reduce model depth and width systematically 
-
         # TODO: Replace global average pooling strategies 
-
         # TODO: Optimize activation functions for edge inference 
-
-        pass 
+        # TODO: Reduce model depth and width systematically 
+        input_layer = self.baseline_model.layers[0]
+        output_layer = self.baseline_model.layers[-1]
+        optimized_model = tf.keras.Sequential()
+        optimized_model.add(input_layer)
+        first_conv = True
+        for layer in self.baseline_model.layers[1:-1]:
+            if isinstance(layer, tf.keras.layers.Conv2D) && first_conv:
+                optimized_model.add(tf.keras.layers.DepthwiseConv2D(filters=layer.filters, 
+                                                                   kernel_size=layer.kernel_size, 
+                                                                   padding=layer.padding))
+                optimized_model.add(tf.keras.layers.BatchNormalization())
+                optimized_model.add(tf.keras.layers.ReLU(max_value=6))
+                first_conv = False
+            elif isinstance(layer, tf.keras.layers.Conv2D) && !first_conv:
+                optimized_model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+                first_conv = True
+            elif isinstance(layer, tf.keras.layers.GlobalAveragePooling2D):
+                optimized_model.add(tf.keras.layers.Flatten())
+            elif isinstance(layer, tf.keras.layers.Dense):
+                optimized_model.add(tf.keras.layers.Dense(units=layer.units/2, activation='relu'))
+            elif isinstance(layer, tf.keras.layers.Dropout):
+                optimalized_model.add(tf.keras.layers.Dropout(rate=layer.rate))
+            else:
+                continue
+        optimalized_model.add(output_layer)    
+        optimized_model.compile(
+            optimizer=keras.optimizers.Adam(learning_rate=0.001),
+            loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=['accuracy']
+        )
+        return optimized_model
 
      
 
@@ -153,9 +218,18 @@ class EdgeOptimizer:
 
         """ 
 
-        # TODO: Define search space (depth, width, kernel sizes) 
-
+        # TODO: Define search space (depth, width, kernel sizes)
+        part3_logger.info(f"Defining search space for architecture optimization")
+        search_space = {
+            'depth': [1,2,3],
+            'width': [64, 128, 256],
+            'kernel_size': [3, 5],
+        }
+        architectures = {}
+        for 
         # TODO: Implement architecture evaluation function 
+        def evaluate_architecture(architecture):
+            
 
         # TODO: Search for Pareto-optimal architectures (accuracy vs efficiency) 
 
